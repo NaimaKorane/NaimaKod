0bff43ba2b99a72a08d2c0dbe457a44e56710913
Naima Korane <naima.korane@gmail.com>
2026-05-20 14:23:33 +0200

    Updated project

--- FizzBuzzApp/obj/Debug/net10.0/FizzBuzzApp.AssemblyInfo.cs
+++ FizzBuzzApp/obj/Debug/net10.0/FizzBuzzApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FizzBuzzApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9b3d4fc16911b0f7eb5aa41a8f87cfc7f34e1a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("FizzBuzzApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FizzBuzzApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
